dbe3e6253ff47b5e655b8873924e8001dd3dce3c
duccanhnguyen <caubebongdem.sp@gmail.com>
2015-12-03 14:55:11 +0700

    NEW

--- QuanLyKhachSan/WindowsFormsApplication1/Properties/Settings.Designer.cs
+++ QuanLyKhachSan/WindowsFormsApplication1/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace WindowsFormsApplication1.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "12.0.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,8 +26,7 @@ namespace WindowsFormsApplication1.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=IDEA-PC\\SQLEXPRESS;Initial Catalog=QuanLyKhachSan;Integrated Security" +
-            "=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=ASUS\\SQLEXPRESS;Initial Catalog=QLKhachSan;Integrated Security=True")]
         public string QLKS {
             get {
                 return ((string)(this["QLKS"]));
